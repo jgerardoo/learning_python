f8384c63b71d1add82b94c4502cdfa3b5dfc4f2a
Jesse Varela <jesse.varela90@gmail.com>
2026-04-20 10:55:28 -0500

    Input simple example

---
+++ code_academy_course/input_simple_example.py
@@ -0,0 +1,6 @@
+favorite_food = input("What is your favorite food? ")
+print("I like " + favorite_food + " too, but i think my favorite food is hotdog")
+print()
+
+their_age = input("What is your age? ")
+print(f"I can't believe you are only {their_age} years old, you look younger!")
